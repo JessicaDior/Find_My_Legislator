607f368ee3b55ddc107b20c92314afde4dcb64b7
Jc Dior <jessicaclairdior@gmail.com>
2021-02-07 19:42:07 +0000

    Initial commit

---
+++ lib/find_my_representative.rb
@@ -0,0 +1,6 @@
+require "find_my_representative/version"
+
+module FindMyRepresentative
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/find_my_representative/version.rb
@@ -0,0 +1,3 @@
+module FindMyRepresentative
+  VERSION = "0.1.0"
+end
